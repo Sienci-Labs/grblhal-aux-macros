760ab09471e01f6a87a59a625fcb17e6068ce560
andrew_marles <andrew@expatria.ca>
2023-11-02 20:51:15 -0700

    Updated macros plugin for B5 revisions.

--- aux_macros.c
+++ aux_macros.c
@@ -199,15 +199,15 @@ static const setting_group_detail_t macro_groups [] = {
 };
 
 static const setting_detail_t macro_settings[] = {
-    { Setting_UserDefined_0, Group_UserSettings, "Run Macro", NULL, Format_String, "x(127)", "0", "127", Setting_NonCore, &plugin_settings.macro[0].data, NULL, NULL },
-    { Setting_UserDefined_1, Group_UserSettings, "Macro 1", NULL, Format_String, "x(127)", "0", "127", Setting_NonCore, &plugin_settings.macro[1].data, NULL, NULL },
-    { Setting_UserDefined_2, Group_UserSettings, "Macro 2", NULL, Format_String, "x(127)", "0", "127", Setting_NonCore, &plugin_settings.macro[2].data, NULL, NULL },
+    { Setting_UserDefined_0, Group_UserSettings, "Macro 1", NULL, Format_String, "x(127)", "0", "127", Setting_NonCore, &plugin_settings.macro[0].data, NULL, NULL },
+    { Setting_UserDefined_1, Group_UserSettings, "Macro 2", NULL, Format_String, "x(127)", "0", "127", Setting_NonCore, &plugin_settings.macro[1].data, NULL, NULL },
+    { Setting_UserDefined_2, Group_UserSettings, "Macro 3", NULL, Format_String, "x(127)", "0", "127", Setting_NonCore, &plugin_settings.macro[2].data, NULL, NULL },
     //{ Setting_UserDefined_3, Group_UserSettings, "Run Real-Time Function", NULL, Format_RadioButtons, "Run,Pause,Halt", NULL, NULL, Setting_NonCore, canbus_set_baud, canbus_get_baud, NULL },
-    { Setting_UserDefined_3, Group_UserSettings, "Run Button Function", NULL, Format_RadioButtons,  "Run Macro,Cycle Start,Pause,Halt,Probe Connected Toggle,Spindle Stop (during pause),Flood Coolant Toggle,Mist Coolant Toggle,Optional Stop Toggle,Single Block Mode Toggle", NULL, NULL, Setting_NonCore, &plugin_settings.macro[0].command_idx, NULL, NULL },
+    { Setting_UserDefined_3, Group_UserSettings, "Macro 1 Button Function", NULL, Format_RadioButtons,  "Run Macro,Cycle Start,Pause,Halt,Probe Connected Toggle,Spindle Stop (during pause),Flood Coolant Toggle,Mist Coolant Toggle,Optional Stop Toggle,Single Block Mode Toggle", NULL, NULL, Setting_NonCore, &plugin_settings.macro[0].command_idx, NULL, NULL },
 
-    { Setting_UserDefined_4, Group_UserSettings, "Macro 1 Button Function", NULL, Format_RadioButtons,  "Macro 1,Cycle Start,Pause,Halt,Probe Connected Toggle,Spindle Stop (during pause),Flood Coolant Toggle,Mist Coolant Toggle,Optional Stop Toggle,Single Block Mode Toggle", NULL, NULL, Setting_NonCore, &plugin_settings.macro[1].command_idx, NULL, NULL },
+    { Setting_UserDefined_4, Group_UserSettings, "Macro 2 Button Function", NULL, Format_RadioButtons,  "Macro 1,Cycle Start,Pause,Halt,Probe Connected Toggle,Spindle Stop (during pause),Flood Coolant Toggle,Mist Coolant Toggle,Optional Stop Toggle,Single Block Mode Toggle", NULL, NULL, Setting_NonCore, &plugin_settings.macro[1].command_idx, NULL, NULL },
 
-    { Setting_UserDefined_5, Group_UserSettings, "Macro 2 Button Function", NULL, Format_RadioButtons,  "Macro 2,Cycle Start,Pause,Halt,Probe Connected Toggle,Spindle Stop (during pause),Flood Coolant Toggle,Mist Coolant Toggle,Optional Stop Toggle,Single Block Mode Toggle", NULL, NULL, Setting_NonCore, &plugin_settings.macro[2].command_idx, NULL, NULL },
+    { Setting_UserDefined_5, Group_UserSettings, "Macro 3 Button Function", NULL, Format_RadioButtons,  "Macro 2,Cycle Start,Pause,Halt,Probe Connected Toggle,Spindle Stop (during pause),Flood Coolant Toggle,Mist Coolant Toggle,Optional Stop Toggle,Single Block Mode Toggle", NULL, NULL, Setting_NonCore, &plugin_settings.macro[2].command_idx, NULL, NULL },
 };
 
 #ifndef NO_SETTINGS_DESCRIPTIONS
@@ -219,11 +219,11 @@ static const setting_descr_t macro_settings_descr[] = {
 
     { Setting_UserDefined_2, "Macro content for macro 3, separate blocks (lines) with the vertical bar character |." },
 
-    { Setting_UserDefined_3, "Real-time function assigned to RUN (overrides macro)"  },
+    { Setting_UserDefined_3, "Real-time function assigned to Macro 1 (overrides macro)"  },
 
-    { Setting_UserDefined_4, "Real-time function assigned to Macro 1 (overrides macro)"  },
+    { Setting_UserDefined_4, "Real-time function assigned to Macro 2 (overrides macro)"  },
 
-    { Setting_UserDefined_5, "Real-time function assigned to Macro 2 (overrides macro)"  },
+    { Setting_UserDefined_5, "Real-time function assigned to Macro 3 (overrides macro)"  },
 
 };
 
@@ -281,7 +281,7 @@ static void macro_settings_load (void)
             pin_info = hal.port.get_pin_info(Port_Digital, Port_Input, port[idx]);
         if(pin_info && !(pin_info->cap.irq_mode & IRQ_Mode_Falling))                // Is port interrupt capable?
             port[idx] = 0xFF;                                                       // No, flag it as not claimed.
-        else if(!ioport_claim(Port_Digital, Port_Input, &port[idx], "Macro 2 pin"))   // Try to claim the port.
+        else if(!ioport_claim(Port_Digital, Port_Input, &port[idx], "Macro 3 pin"))   // Try to claim the port.
             port[idx] = 0xFF;                                                       // If not successful flag it as not claimed.
 
         idx--;
@@ -290,7 +290,7 @@ static void macro_settings_load (void)
             pin_info = hal.port.get_pin_info(Port_Digital, Port_Input, port[idx]);
         if(pin_info && !(pin_info->cap.irq_mode & IRQ_Mode_Falling))                // Is port interrupt capable?
             port[idx] = 0xFF;                                                       // No, flag it as not claimed.
-        else if(!ioport_claim(Port_Digital, Port_Input, &port[idx], "Macro 1 pin"))   // Try to claim the port.
+        else if(!ioport_claim(Port_Digital, Port_Input, &port[idx], "Macro 2 pin"))   // Try to claim the port.
             port[idx] = 0xFF;    
 
         idx--;
@@ -299,7 +299,7 @@ static void macro_settings_load (void)
             pin_info = hal.port.get_pin_info(Port_Digital, Port_Input, port[idx]);
         if(pin_info && !(pin_info->cap.irq_mode & IRQ_Mode_Falling))                // Is port interrupt capable?
             port[idx] = 0xFF;                                                       // No, flag it as not claimed.
-        else if(!ioport_claim(Port_Digital, Port_Input, &port[idx], "Run pin"))   // Try to claim the port.
+        else if(!ioport_claim(Port_Digital, Port_Input, &port[idx], "Macro 1 pin"))   // Try to claim the port.
             port[idx] = 0xFF;                         
     }
 
